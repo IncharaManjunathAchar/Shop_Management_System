1665f998c8cfe4cd212141c655b660388e2140e4
harshitha29-sky <harshithaharshitha6381@gmail.com>
2026-03-26 10:08:54 -0700

    updated admin

--- frontend/src/app/components/Admin/dashboard/dashboard.component.ts
+++ frontend/src/app/components/Admin/dashboard/dashboard.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
 
+  subscriptions: any[] = [];
+
+  private apiUrl = 'https://localhost:5001/api/admin/usersubscriptions';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getSubscriptions();
+  }
+
+  getSubscriptions() {
+    this.http.get<any[]>(this.apiUrl).subscribe({
+      next: (res) => {
+        this.subscriptions = res;
+      },
+      error: (err) => {
+        console.error('Error fetching subscriptions', err);
+      }
+    });
+  }
 }
